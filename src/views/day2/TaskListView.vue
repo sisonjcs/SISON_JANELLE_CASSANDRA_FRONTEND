9dafe26e72fb04a26a6bf720f9ab2262f725722d
sisonjcs <jcssison1@gmail.com>
2026-06-17 10:31:21 +0800

    fix: fixed imports

--- src/views/day2/TaskListView.vue
+++ src/views/day2/TaskListView.vue
@@ -6,7 +6,7 @@
 -->
 <script setup>
 import { ref } from 'vue'
-import TaskCard from './TaskCard.vue'
+import TaskCard from '../../components/TaskCard.vue'
 
 // TODO 1: Create a ref() tasks array with at least 3 sample tasks
 // Each task: { id, name, done, dueDate }
