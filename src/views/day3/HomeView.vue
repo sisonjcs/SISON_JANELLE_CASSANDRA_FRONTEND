3945e7bf0f7f3f55d0006c5a1fa0ddba0fb6f81d
sisonjcs <jcssison1@gmail.com>
2026-06-17 16:25:15 +0800

    feat: clickable item list

--- src/views/day3/HomeView.vue
+++ src/views/day3/HomeView.vue
@@ -32,12 +32,11 @@ const showErrorBanner = computed(() => route.query.error === 'notfound')
       <!-- TODO 4: Render each task as a RouterLink to /task/:id -->
       <!-- Use <RouterLink :to="`/task/${task.id}`"> as the wrapper -->
       <ul class="task-list">
-        <li v-for="task in taskStore.tasks" :key="task.id">
-          <!-- TODO 5: Wrap this in a RouterLink -->
-           <RouterLink :to="`/task/${task.id}`">
+        <RouterLink  v-for="task in taskStore.tasks" :key="task.id" :to="`/task/${task.id}`">
+          <li>
             <span :class="{ done: task.done }">{{ task.name }}</span>
-           </RouterLink>
-        </li>
+          </li>
+        </RouterLink>
       </ul>
     </div>
   </div>
@@ -46,12 +45,13 @@ const showErrorBanner = computed(() => route.query.error === 'notfound')
 <style scoped>
 .home-view { max-width: 520px; margin: 40px auto; font-family: Arial, sans-serif; }
 .home-content { margin-top: 50px;}
-h1 { color: #1B2A4A; margin-bottom: 16px; }
+h1 { color: #1B2A4A; margin-bottom: 16px; font-weight: bold; text-align: center;}
 .error-banner { background: #fef3c7; border: 1px solid #f59e0b; border-radius: 6px; padding: 10px 14px; margin-bottom: 16px; color: #92400e; font-size: 14px; }
 .page-nav { margin-bottom: 20px; }
 .page-nav a { color: #42B883; text-decoration: none; font-weight: 600; }
 .task-list { list-style: none; padding: 0; margin: 0; }
-.task-list li { padding: 12px; background: white; border-radius: 8px; margin-bottom: 8px; border: 1px solid #eee; cursor: pointer; }
-.task-list li:hover { border-color: #42B883; }
+.task-list li { padding: 12px; background: white; border-radius: 8px; border: 1px solid #eee; cursor: pointer; box-shadow: 0 1px 5px rgba(0,0,0,0.1); transition: all 0.2s ease;}
+.task-list li:hover { border-color: #42B883; transform: translateY(-5px); box-shadow: 0 2px 5px rgba(0,0,0,0.2); }
 .done { text-decoration: line-through; color: #9ca3af; }
 </style>
+           
